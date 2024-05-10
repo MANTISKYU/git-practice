e55b74ba060a8f96582aadd22a4c0b5ee298fbe4
MANTISKYU <asdaron44@gmail.com>
2024-05-10 11:09:53 +0900

    출력 문자 변경

--- src/main/java/ohgiraffers/example.java
+++ src/main/java/ohgiraffers/example.java
@@ -4,7 +4,7 @@ public class example {
     public static void main(String[] args) {
 
         System.out.print("Hello world!");
-        System.out.print("한글은 안 깨져요");
+        System.out.print("점심은 곱창 쌀국수다 으하하");
 
     }
 }
